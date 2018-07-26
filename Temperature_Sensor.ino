ffca230764d9aa893e8c74b2a52f166db48b0e25
Mason Gill <28371646+MasonGill@users.noreply.github.com>
2018-07-26 14:50:01 +1200

    Changed some variable names, fixed a bug with a char array
    Changed variable names to improve readability
    Fixed bug with const char arrays where they would either display ridiculously large numbers or no numbers at all.
    Allocated less memory for the returned char array in tempBuild function.

--- Temperature_Sensor.ino
+++ Temperature_Sensor.ino
@@ -4,13 +4,13 @@
 #include <TimerOne.h>
 #include "Arial14.h"
 
-DMD dmd(1,1);
+DMD dmd(1, 1);
 
 double tempRead(int analog)
 {
   double temp;
-  temp = log(((10240000/analog) - 10000));
-  temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * temp * temp ))* temp );
+  temp = log(((10240000 / analog) - 10000));
+  temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * temp * temp )) * temp );
   temp -= 273.15;
   return temp;
 }
@@ -18,26 +18,26 @@ double tempRead(int analog)
 char *tempBuild()
 {
   double f = tempRead(analogRead(0));
-  int h = f;
-  int fPoint = (f - (double)h)*10;
-  const char* d = "c";
-  const char* j = ".";
-  const char b[strlen(d)];
-  const char q[strlen(d)];
-  itoa(h, b, 10);
-  itoa(fPoint, q, 10);
-  char* c = malloc(64);
-  strcpy(c, b);
-  strcat(c, j);
-  strcat(c, q);
-  strcat(c, d);
-  return c;
+  int fullNum = f;
+  int fPoint = (f - (double)fullNum) * 10;
+  const char* letter = "c";
+  const char* dot = ".";
+  const char beforep[4];
+  const char afterp[2];
+  itoa(fullNum, beforep, 10);
+  itoa(fPoint, afterp, 10);
+  char* temp = malloc(16);
+  strcpy(temp, beforep);
+  strcat(temp, dot);
+  strcat(temp, afterp);
+  strcat(temp, letter);
+  return temp;
 }
 
 void(* resetFunc) (void) = 0;
 
 void scan()
-{ 
+{
   dmd.scanDisplayBySPI();
 }
 
@@ -48,11 +48,11 @@ void setup(void)
   dmd.selectFont(Arial_14);
   Serial.begin(9600);
   char *temp = tempBuild();
+  Serial.print(temp);
   dmd.drawString( 1, 2, temp, strlen(temp), GRAPHICS_TOGGLE);
   delay(2000);
   dmd.clearScreen(true);
-  Serial.print(temp);
   resetFunc();
 }
 
-void loop(void){}
+void loop(void) {}
